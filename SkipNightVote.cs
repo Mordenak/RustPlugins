ada809c662d551164abec4dcc6bc17147c276cc2
Mordenak <nicksolwick@gmail.com>
2015-06-04 20:44:08 -0700

    Version 1.1.0, moved messages out to config, re-organized config and added version checking to prevent bad config rollovers.

--- SkipNightVote.cs
+++ SkipNightVote.cs
@@ -9,7 +9,7 @@ using Oxide.Core.Plugins;
 
 
 namespace Oxide.Plugins {
-    [Info("SkipNightVote", "Mordenak", "1.0.6", ResourceId = 1014)]
+    [Info("SkipNightVote", "Mordenak", "1.1.0", ResourceId = 1014)]
     class SkipNightVote : RustPlugin {
 
         public class TimePoll : RustPlugin {
@@ -29,10 +29,8 @@ namespace Oxide.Plugins {
             public bool voteDay(BasePlayer player) {
                 var playerId = player.userID.ToString();
 
-                if (!checkVote(playerId)) {
-                    SendReply(player, "You have already voted once.");
+                if (!checkVote(playerId))
                     return false;
-                }
                 votesReceived.Add(playerId, true);
                 return true;
             }
@@ -58,7 +56,7 @@ namespace Oxide.Plugins {
 
         public float requiredVotesPercentage = 0.5f; // % of votes needed to change time
         public float pollRetryTime = 5; // in minutes
-        public float pollTimer = 0.5f; // in minutes
+        public float pollTimer = 1.0f; // in minutes
         public int sunsetHour = 18; // hour to start a vote
         public int sunriseHour = 8; // hour to set if vote is successful
         public bool displayVoteProgress = false; // determine whether to display a message for vote progress
@@ -67,24 +65,37 @@ namespace Oxide.Plugins {
         
         public TimePoll votePoll = null;
         float lastPoll = 0f;
+
+        // Messages!
+        string noPollOpen = "No poll is open at this time.";
+        string alreadyVoted = "You have already voted once.";
+        string voteProgress = "Vote progress: {0} / {1} ({2}%/{3}%)";
+        string voteOpenTime = "Night time skip vote is now open for {0} minute(s).";
+        string voteSuccessful = "Vote was successful, it will be daytime soon.";
+        string voteFailed = "Vote failed, not enough players voted to skip night time.";
+        string voteReOpenTime = "Vote will re-open in {0} minute(s).";
+        string voteNow = "Type <color=#FF2211>/voteday</color> now to skip night time.";
         
 
         [ChatCommand("voteday")]
         void cmdVoteTime(BasePlayer player, string command, string[] args) {
             if (votePoll == null) {
-                SendReply(player, "No poll is open at this time.");
+                SendReply(player, noPollOpen);
                 return;
             }
 
             var checkVote = votePoll.voteDay(player);
-            if (!checkVote) return; // don't go further if the player has voted
+            if (!checkVote) {
+                SendReply(player, alreadyVoted);
+                return; // don't go further if the player has voted
+            }
             checkVotes();
             if (displayVoteProgress) {
                 if (votePoll != null) {
                     int totalPlayers = BasePlayer.activePlayerList.Count;
                     int votes = votePoll.tallyVotes();
                     float percent = (float)votes / totalPlayers;
-                    MessageAllPlayers( string.Format("Vote progress: {0} / {1} ({2}%/{3}%)", votes, totalPlayers, (int)(percent*100), (int)(requiredVotesPercentage*100)) );
+                    MessageAllPlayers( string.Format(voteProgress, votes, totalPlayers, (int)(percent*100), (int)(requiredVotesPercentage*100)) );
                 }
             }
         }
@@ -95,8 +106,8 @@ namespace Oxide.Plugins {
             if (votePoll != null) 
                 return;
             votePoll = new TimePoll(requiredVotesPercentage);
-            MessageAllPlayers(string.Format("Night time skip vote is now open for {0} minute(s).", pollTimer) );
-            MessageAllPlayers("Type <color=#FF2211>/voteday</color> now to skip night time.");
+            MessageAllPlayers(string.Format(voteOpenTime, pollTimer) );
+            MessageAllPlayers(voteNow);
             lastPoll = Time.realtimeSinceStartup;
         }
 
@@ -106,7 +117,7 @@ namespace Oxide.Plugins {
 
         void checkVotes() {
             if (votePoll.wasVoteSuccessful()) {
-                MessageAllPlayers("Vote was successful, it will be daytime soon.");
+                MessageAllPlayers(voteSuccessful);
                 // change time
                 TOD_Sky.Instance.Cycle.Hour = sunriseHour;
                 Puts("{0}: {1}", Title, "has changed the server time.");
@@ -122,8 +133,8 @@ namespace Oxide.Plugins {
                     //Debug.Log("Plugin passed ready check...");
                     if (votePoll != null) { // timeout
                         if (Time.realtimeSinceStartup >= (lastPoll + (pollTimer * 60))) {
-                            MessageAllPlayers("Vote failed, not enough players voted to skip night time." );
-                            MessageAllPlayers(string.Format("Vote will re-open in {0} minute(s).", pollRetryTime) );
+                            MessageAllPlayers( voteFailed );
+                            MessageAllPlayers(string.Format(voteReOpenTime, pollRetryTime) );
                             closeVote();
                         }
                     }
@@ -153,39 +164,91 @@ namespace Oxide.Plugins {
         }
 
         void PopulateConfig() {
-            Config["requiredVotesPercentage"] = requiredVotesPercentage;
-            Config["pollRetryTime"] = pollRetryTime;
-            Config["pollTimer"] = pollTimer;
-            Config["sunsetHour"] = sunsetHour;
-            Config["sunriseHour"] = sunriseHour;
-            Config["displayVoteProgress"] = displayVoteProgress;
+
+            Config["Version"] = Version.ToString();
+
+            var settings = new Dictionary<string, object>();
+            settings.Add("requiredVotesPercentage", requiredVotesPercentage);
+            settings.Add("pollRetryTime", pollRetryTime);
+            settings.Add("pollTimer", pollTimer);
+            settings.Add("sunsetHour", sunsetHour);
+            settings.Add("sunriseHour", sunriseHour);
+            settings.Add("displayVoteProgress", displayVoteProgress);
+
+            Config["Settings"] = settings;
+
+            // messages
+            var messages = new Dictionary<string, string>();
+            messages.Add("noPollOpen", "No poll is open at this time.");
+            messages.Add("alreadyVoted", "You have already voted once.");
+            messages.Add("voteProgress", "Vote progress: {0} / {1} ({2}%/{3}%)");
+            messages.Add("voteOpenTime", "Night time skip vote is now open for {0} minute(s).");
+            messages.Add("voteSuccessful", "Vote was successful, it will be daytime soon.");
+            messages.Add("voteFailed", "Vote failed, not enough players voted to skip night time.");
+            messages.Add("voteReOpenTime", "Vote will re-open in {0} minute(s).");
+            messages.Add("voteNow", "Type <color=#FF2211>/voteday</color> now to skip night time.");
+
+            Config["Messages"] = messages;
+
             SaveConfig();
         }
 
         void Loaded() {
             LoadConfig();
 
-            if (Config["requiredVotesPercentage"] != null ) 
-                requiredVotesPercentage = (float)Convert.ChangeType(Config["requiredVotesPercentage"], typeof(float));
+            Debug.Log( string.Format("version is {0}", Version) );
 
-            if (Config["pollRetryTime"] != null )
-                pollRetryTime = (float)Convert.ChangeType(Config["pollRetryTime"], typeof(float));
-            
-            if (Config["pollTimer"] != null )
-                pollTimer = (float)Convert.ChangeType(Config["pollTimer"], typeof(float));
-            
-            if (Config["sunsetHour"] != null )
-                sunsetHour = (int)Config["sunsetHour"];
+            if (Config["Version"] != null) {
+                var cfgVersion = Config["Version"] as string;
+                if (cfgVersion != Version.ToString()) {
+                    PrintError("{0}: {1}", Title, "Config out of date!  Forcing update.");
+                    Config.Clear();
+                    PopulateConfig();
+                    readyToCheck = true;
+                }
+                else {
+                    if (Config["Settings"] != null) {
+                        var settings = Config["Settings"] as Dictionary<string, object>;
 
-            if (Config["sunriseHour"] != null )
-                sunriseHour = (int)Config["sunriseHour"];
+                        requiredVotesPercentage = (float)Convert.ChangeType(settings["requiredVotesPercentage"], typeof(float));
+                        pollRetryTime = (float)Convert.ChangeType(settings["pollRetryTime"], typeof(float));
+                        pollTimer = (float)Convert.ChangeType(settings["pollTimer"], typeof(float));
+                        sunsetHour = (int)settings["sunsetHour"];
+                        sunriseHour = (int)settings["sunriseHour"];
+                        displayVoteProgress = (bool)settings["displayVoteProgress"];
+                    }
+                    else {
+                        PrintError("{0}: {1}", Title, "Loading Config[\"Settings\"] failed.");
+                    }
 
-            if (Config["displayVoteProgress"] != null )
-                displayVoteProgress = (bool)Config["displayVoteProgress"];
+                    if (Config["Messages"] != null) {
+                        var messages = Config["Messages"] as Dictionary<string, object>;
 
-            readyToCheck = true;
-            // it appears we don't want to get this too early...
-            PopulateConfig();
+                        
+                        noPollOpen = (string)messages["noPollOpen"];
+                        alreadyVoted = (string)messages["alreadyVoted"];
+                        voteProgress = (string)messages["voteProgress"];
+                        voteOpenTime = (string)messages["voteOpenTime"];
+                        voteSuccessful = (string)messages["voteSuccessful"];
+                        voteFailed = (string)messages["voteFailed"];
+                        voteReOpenTime = (string)messages["voteReOpenTime"];
+                        voteNow = (string)messages["voteNow"];
+                    }
+                    else {
+                        PrintError("{0}: {1}", Title, "Loading Config[\"Messages\"] failed.");
+                    }
+                    
+                    readyToCheck = true;
+                    // it appears we don't want to get this too early...
+                    PopulateConfig();
+                }
+            }
+            else {
+                PrintError("{0}: {1}", Title, "Config out of date!  Forcing update.");
+                Config.Clear();
+                PopulateConfig();
+                readyToCheck = true;
+            }
         }
 
 
